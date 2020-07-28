03c73ed31454fc43d5b2f203f1ed7af3a9de4273
DalyAD <Addaly91@gmail.com>
2020-07-28 19:16:18 +0000

    Added category filtering

--- books/models.py
+++ books/models.py
@@ -17,11 +17,11 @@ class Category(models.Model):
 
 
 class Book(models.Model):
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
     isbn13 = models.CharField(max_length=254, null=True, blank=True)
     isbn10 = models.CharField(max_length=254, null=True, blank=True)
     title = models.CharField(max_length=254)
     author = models.CharField(max_length=254)
-    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
     image = models.ImageField(null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
