3a5b40ebda0857c2fc6bca484830786060954994
DalyAD <Addaly91@gmail.com>
2020-07-28 18:50:20 +0000

    added search functionality

--- books/views.py
+++ books/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect, reverse
+from django.contrib import messages
+from django.db.models import Q
 from .models import Book
 
 
@@ -6,9 +8,32 @@ def all_books(request):
     """ A view to return all book, including sorting and search queries """
 
     books = Book.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+        if not query:
+            messages.error(request, "You didn't enter any search criteria!")
+            return redirect(reverse('products'))
+
+        queries = Q(title__icontains=query) | Q(description__icontains=query) | Q(author__icontains=query)
+        books = books.filter(queries)
 
     context = {
         'books': books,
     }
 
     return render(request, 'books/books.html', context)
+
+
+def book_detail(request, book_id):
+    """ A view to show individual book details """
+
+    book = get_object_or_404(Book, pk=book_id)
+
+    context = {
+        'book': book,
+    }
+
+    return render(request, 'books/book_detail.html', context)
