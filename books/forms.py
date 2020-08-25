62dcdb23cb4ec95493205f3794a8f2dc67286fbf
DalyAD <Addaly91@gmail.com>
2020-08-25 12:38:38 +0000

    fixed the image field

--- books/forms.py
+++ books/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from .models import Book, Category
+from .widgets import CustomClearableFileInput
 
 
 class BookForm(forms.ModelForm):
@@ -7,6 +8,8 @@ class BookForm(forms.ModelForm):
     class Meta:
         model = Book
         fields = '__all__'
+
+    image = forms.ImageField(label='Image', required=False, widget=CustomClearableFileInput)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
