f0018239e383ff9dc6bc891a165b7112c24a0a4b
DalyAD <Addaly91@gmail.com>
2020-09-01 17:10:12 +0000

    added model for authors

--- books/models.py
+++ books/models.py
@@ -16,13 +16,22 @@ class Category(models.Model):
         return self.friendly_name
 
 
+class Author(models.Model):
+
+    name = models.CharField(max_length=254, null=True)
+
+    def __str__(self):
+        return self.name
+
+
 class Book(models.Model):
     category = models.ForeignKey('Category', null=True,
                                  blank=True, on_delete=models.SET_NULL)
     isbn13 = models.CharField(max_length=254, null=True, blank=True)
     isbn10 = models.CharField(max_length=254, null=True, blank=True)
     title = models.CharField(max_length=254)
-    author = models.CharField(max_length=254)
+    author = models.ForeignKey('Author', null=True,
+                                 blank=True, on_delete=models.SET_NULL)
     image = models.ImageField(null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
